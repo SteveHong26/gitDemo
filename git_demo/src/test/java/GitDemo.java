bd35e82a61e97c21405277ee8241677ddc52ddf3
Steve <SteveHZL26@outllook.com>
2018-11-24 19:56:22 +0800

    Modify

--- git_demo/src/test/java/GitDemo.java
+++ git_demo/src/test/java/GitDemo.java
@@ -2,6 +2,7 @@ public class GitDemo {
     public static void main(String[] args) {
             System.out.println("One");
             System.out.println("Two");
+            System.out.println("Three")
     }
 
 }

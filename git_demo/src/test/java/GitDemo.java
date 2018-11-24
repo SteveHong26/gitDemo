d20adf5e3c860dc9d2079489e4a6d99cfa9be447
Steve <SteveHZL26@outllook.com>
2018-11-24 20:00:54 +0800

    Conflict

--- git_demo/src/test/java/GitDemo.java
+++ git_demo/src/test/java/GitDemo.java
@@ -2,7 +2,8 @@ public class GitDemo {
     public static void main(String[] args) {
             System.out.println("One");
             System.out.println("Two");
-            System.out.println("Three")
+        System.out.println("Three");
+        System.out.println("Idea");
     }
 
 }
